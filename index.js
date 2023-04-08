35d28e4ca5ea4d7041f441017c608445cabe00c6
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-04-08 01:02:20 -0400

    Added input validations

--- index.js
+++ index.js
@@ -26,57 +26,59 @@ const generateHTML = (arr) => {
                         </ul>
                     </div>`
             } else if (employee.getRole() === 'Engineer') {
-                employeeCards += `    <div class="card m-2" style="width: 18rem;">
-                <div class="card-header">
-                    <h3>${employee.name}</h3>
-                    <p class="fs-5">Engineer</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${employee.id}</li>
-                    <li class="list-group-item">Email: ${employee.email}</li>
-                    <li class="list-group-item">github: ${employee.github}</li>
-                </ul>
-            </div>`
+                employeeCards += 
+                `<div class="card m-2" style="width: 18rem;">
+                    <div class="card-header">
+                        <h3>${employee.name}</h3>
+                        <p class="fs-5">Engineer</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${employee.id}</li>
+                        <li class="list-group-item">Email: ${employee.email}</li>
+                        <li class="list-group-item">github: ${employee.github}</li>
+                    </ul>
+                </div>`
             } else {
-                employeeCards += `    <div class="card m-2" style="width: 18rem;">
-                <div class="card-header">
-                    <h3>${employee.name}</h3>
-                    <p class="fs-5">Intern</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${employee.id}</li>
-                    <li class="list-group-item">Email: ${employee.email}</li>
-                    <li class="list-group-item">School: ${employee.school}</li>
-                </ul>
-            </div>`
+                employeeCards += `
+                <div class="card m-2" style="width: 18rem;">
+                    <div class="card-header">
+                        <h3>${employee.name}</h3>
+                        <p class="fs-5">Intern</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${employee.id}</li>
+                        <li class="list-group-item">Email: ${employee.email}</li>
+                        <li class="list-group-item">School: ${employee.school}</li>
+                    </ul>
+                </div>`
             }
         }
     )
     
     return `
     <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-        <link href="./style.css" rel="stylesheet">
-        <title>Employees</title>
-    </head>
-    <body>
-        <div class="container-fluid p-0">
-            <nav class="col-12 p-4 bg-success text-center">
-                <h1 class="text-white">Company Name</h1>
-            </nav>
-            <div class="container d-flex flex-row flex-wrap justify-content-evenly mt-4">
-                ${employeeCards}
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+            <link href="./style.css" rel="stylesheet">
+            <title>Employees</title>
+        </head>
+        <body>
+            <div class="container-fluid p-0">
+                <nav class="col-12 p-4 bg-success text-center">
+                    <h1 class="text-white">Company Name</h1>
+                </nav>
+                <div class="container d-flex flex-row flex-wrap justify-content-evenly mt-4">
+                    ${employeeCards}
+                </div>
             </div>
-        </div>
-    </body>
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-</html>
+        </body>
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    </html>
     `;
 };
 
@@ -99,7 +101,7 @@ function generateEmployees () {
                 break  
             case 'Finished': 
                 fs.writeFile('./dist/index.html', generateHTML(employeeArray), (err) => 
-                err?console.log(err):console.log('Success!'))
+                err?console.log(err):console.log('Success! Check \'index.html\' under the \'dist\' folder.'))
         }
     })
 } 
@@ -111,21 +113,55 @@ function generateManager () {
             type: 'input',
             name: 'employeeName',
             message: 'Please enter the manager\'s name',
+            validate: (name) => {
+                if (name) {
+                  return true;
+                } else {
+                  console.log("Please enter the name of this employee");
+                  return false;
+                }
+              },
         },
         {
             type: 'input',
             name: 'id',
             message: 'Please enter their ID number',
+            validate: (id) => {
+                if (!isNaN(id)) {
+                  return true;
+                } else {
+                  console.log("Please enter a valid ID number");
+                  return false;
+                }
+              },
         },
         {
             type: 'input',
             name: 'email',
             message: 'Please enter their email',
+            //checks if valid email
+            validate: (email) => {
+                const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (valid) {
+                  return true;
+                } else {
+                  console.log("Please enter a valid email!");
+                  return false;
+                }
+              },
         },
         {
             type: 'input',
             name: 'officeNum',
             message: 'Please enter their office number',
+            validate: (id) => {
+                if (!isNaN(id)) {
+                  return true;
+                } else {
+                  console.log("Please enter a valid office number");
+                  return false;
+                }
+              },
         },
     ]).then (newManager => {
         const {name, id, email, officeNum} = newManager;
@@ -143,21 +179,55 @@ function generateEngineer () {
             type: 'input',
             name: 'name',
             message: 'Please enter the engineers\'s name',
+            validate: (name) => {
+                if (name) {
+                  return true;
+                } else {
+                  console.log("Please enter the name of this employee");
+                  return false;
+                }
+              },
         },
         {
             type: 'input',
             name: 'id',
             message: 'Please enter their id number',
+            validate: (id) => {
+                if (!isNaN(id)) {
+                  return true;
+                } else {
+                  console.log("Please enter a valid ID number");
+                  return false;
+                }
+              },
         },
         {
             type: 'input',
             name: 'email',
             message: 'Please enter their email',
+            //checks if valid email
+            validate: (email) => {
+                const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid email");
+                    return false;
+                }
+                },
         },
         {
             type: 'input',
             name: 'github',
             message: 'Please enter their github profile name',
+            validate: (github) => {
+                if (github) {
+                  return true;
+                } else {
+                  console.log("Please enter the github profile name of this engineer");
+                  return false;
+                }
+              },
         },
     ]).then (newEngineer => {
         const {name, id, email, github} = newEngineer;
@@ -175,21 +245,56 @@ function generateIntern () {
             type: 'input',
             name: 'name',
             message: 'Please enter the engineers\'s name',
+            validate: (name) => {
+                if (name) {
+                  return true;
+                } else {
+                  console.log("Please enter the name of this employee");
+                  return false;
+                }
+              },
         },
         {
             type: 'input',
             name: 'id',
             message: 'Please enter their id number',
+            validate: (id) => {
+                if (!isNaN(id)) {
+                  return true;
+                } else {
+                  console.log("Please enter a valid ID number");
+                  return false;
+                }
+              },
         },
         {
             type: 'input',
             name: 'email',
             message: 'Please enter their email',
+            //checks if valid email
+            validate: (email) => {
+                const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid email");
+                    return false;
+                }
+            },
         },
         {
             type: 'input',
             name: 'school',
             message: 'Please enter their school name',
+            validate: (school) => {
+                if (school) {
+                  return true;
+                } else {
+                  console.log("Please enter the name of the school");
+                  return false;
+                }
+              },
+            
         },
     ]).then (newIntern => {
         const {name, id, email, school} = newIntern;
